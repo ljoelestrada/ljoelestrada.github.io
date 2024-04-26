84fd5210288b69cface0f56eac34187835a5dcc5
ljoelestrada <165860011+ljoelestrada@users.noreply.github.com>
2024-04-26 11:52:39 -0600

    Add files via upload

---
+++ parcial3/format.js
@@ -0,0 +1,53 @@
+// PRIMERA SEMANA
+function ej1_1(){
+    document.getElementById("btn1").innerHTML = "<a id='vinculo' href='iniciales/variables.html' target='iframe1'>1. Variables</a>"
+}
+function ej1_2(){
+    document.getElementById("btn1").innerHTML = "<a id='vinculo' href='iniciales/parrafos.html' target='iframe1'>2. Parrafos</a>"
+}
+function ej1_3(){
+    document.getElementById("btn1").innerHTML = "<a id='vinculo' href='iniciales/tabla.html' target='iframe1'>3. Tabla</a>"
+}
+function ej1_4(){
+    document.getElementById("btn1").innerHTML = "<a id='vinculo' href='iniciales/imagen.html' target='iframe1'>4. Imagenes</a>"
+}
+function ej1_5(){
+    document.getElementById("btn1").innerHTML = "<a id='vinculo' href='iniciales/char.html' target='iframe1'>5. Caracter</a>"
+}
+function ej1_6(){
+    document.getElementById("btn1").innerHTML = "<a id='vinculo' href='iniciales/aleatorios.html' target='iframe1'>6. Aleatorios</a>"
+}
+function ej1_7(){
+    document.getElementById("btn1").innerHTML = "<a id='vinculo' href='iniciales/login.html' target='iframe1'>7. Login</a>"
+}
+function ej1_8(){
+    document.getElementById("btn1").innerHTML = "<a id='vinculo' href='iniciales/conversor.html' target='iframe1'>8. Conversor</a>"
+}
+function ej1_9(){
+    document.getElementById("btn1").innerHTML = "<a id='vinculo' href='iniciales/arreglos.html' target='iframe1'>9. Arreglos</a>"
+}
+// SEGUNDA SEMANA
+function ej2_1(){
+    document.getElementById("btn2").innerHTML = "<a id='vinculo' href='apis/geo.html' target='iframe2'>1. Geolocalizacion</a>"
+}
+function ej2_2(){
+    document.getElementById("btn2").innerHTML = "<a id='vinculo' href='apis/mapa.html' target='iframe2'>2. Mapa</a>"
+}
+function ej2_3(){
+    document.getElementById("btn2").innerHTML = "<a id='vinculo' href='apis/alert.html' target='iframe2'>3. Alerta</a>"
+}
+function ej2_4(){
+    document.getElementById("btn2").innerHTML = "<a id='vinculo' href='apis/canva.html' target='iframe2'>4. Barras</a>"
+}
+function ej2_5(){
+    document.getElementById("btn2").innerHTML = "<a id='vinculo' href='apis/graficas.html' target='iframe2'>5. Graficas</a>"
+}
+function ej2_6(){
+    document.getElementById("btn2").innerHTML = "<a id='vinculo' href='apis/colores.html' target='iframe2'>6. Colores</a>"
+}
+function ej2_7(){
+    document.getElementById("btn2").innerHTML = "<a id='vinculo' href='apis/reto1.html' target='iframe2'>7. Graficas 1</a>"
+}
+function ej2_8(){
+    document.getElementById("btn2").innerHTML = "<a id='vinculo' href='apis/reto2.html' target='iframe2'>8. Graficas 2</a>"
+}
