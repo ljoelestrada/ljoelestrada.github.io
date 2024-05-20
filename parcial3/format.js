06695cf975ac540607bb904d97360d0f4708d13b
ljoelestrada <165860011+ljoelestrada@users.noreply.github.com>
2024-05-20 06:28:06 -0600

    Add files via upload

---
+++ parcial3/format.js
@@ -0,0 +1,76 @@
+// PRIMERA SEMANA
+function ej1_1(){
+    document.getElementById("btn1").innerHTML = "<a id='vinculo' href='iniciales/variables.html' target='iframe1'>1. Variables</a>"
+}
+function ej1_2(){
+    document.getElementById("btn1").innerHTML = "<a id='vinculo' href='iniciales/parrafos.html' target='iframe1'>2. Parrafos</a>"
+}
+function ej1_3(){
+    document.getElementById("btn1").innerHTML = "<a id='vinculo' href='iniciales/tabla.html' target='iframe1'>3. Tabla</a>"
+}
+function ej1_4(){
+    document.getElementById("btn1").innerHTML = "<a id='vinculo' href='iniciales/imagen.html' target='iframe1'>4. Imagenes</a>"
+}
+function ej1_5(){
+    document.getElementById("btn1").innerHTML = "<a id='vinculo' href='iniciales/char.html' target='iframe1'>5. Caracter</a>"
+}
+function ej1_6(){
+    document.getElementById("btn1").innerHTML = "<a id='vinculo' href='iniciales/aleatorios.html' target='iframe1'>6. Aleatorios</a>"
+}
+function ej1_7(){
+    document.getElementById("btn1").innerHTML = "<a id='vinculo' href='iniciales/login.html' target='iframe1'>7. Login</a>"
+}
+function ej1_8(){
+    document.getElementById("btn1").innerHTML = "<a id='vinculo' href='iniciales/conversor.html' target='iframe1'>8. Conversor</a>"
+}
+function ej1_9(){
+    document.getElementById("btn1").innerHTML = "<a id='vinculo' href='iniciales/arreglos.html' target='iframe1'>9. Arreglos</a>"
+}
+// SEGUNDA SEMANA
+function ej2_1(){
+    document.getElementById("btn2").innerHTML = "<a id='vinculo' href='apis/geo.html' target='iframe2'>1. Geolocalizacion</a>"
+}
+function ej2_2(){
+    document.getElementById("btn2").innerHTML = "<a id='vinculo' href='apis/mapa.html' target='iframe2'>2. Mapa</a>"
+}
+function ej2_3(){
+    document.getElementById("btn2").innerHTML = "<a id='vinculo' href='apis/alert.html' target='iframe2'>3. Alerta</a>"
+}
+function ej2_4(){
+    document.getElementById("btn2").innerHTML = "<a id='vinculo' href='apis/canva.html' target='iframe2'>4. Barras</a>"
+}
+function ej2_5(){
+    document.getElementById("btn2").innerHTML = "<a id='vinculo' href='apis/graficas.html' target='iframe2'>5. Graficas</a>"
+}
+function ej2_6(){
+    document.getElementById("btn2").innerHTML = "<a id='vinculo' href='apis/colores.html' target='iframe2'>6. Colores</a>"
+}
+function ej2_7(){
+    document.getElementById("btn2").innerHTML = "<a id='vinculo' href='apis/reto1.html' target='iframe2'>7. Graficas 1</a>"
+}
+function ej2_8(){
+    document.getElementById("btn2").innerHTML = "<a id='vinculo' href='apis/reto2.html' target='iframe2'>8. Graficas 2</a>"
+}
+// TERCERA SEMANA
+function ej3_1(){
+    document.getElementById("btn3").innerHTML = "<a id='vinculo' href='ahorcado.html' target='iframe3'>1. Ahorcado</a>"
+}
+function ej3_2(){
+    document.getElementById("btn3").innerHTML = "<a id='vinculo' href='test/testalumno.html' target='iframe3'>2. Formulario</a>"
+}
+// CUARTA SEMANA
+function ej4_1(){
+    document.getElementById("btn4").innerHTML = "<a id='vinculo' href='test/jspdf.html' target='iframe4'>1. Imagen PDF</a>"
+}
+function ej4_2(){
+    document.getElementById("btn4").innerHTML = "<a id='vinculo' href='test/ejemplopdf.html' target='iframe4'>2. Operaciones basicas PDF</a>"
+}
+function ej4_3(){
+    document.getElementById("btn4").innerHTML = "<a id='vinculo' href='test/ejerciciopdf2.html' target='iframe4'>3. Tablas PDF</a>"
+}
+function ej4_4(){
+    document.getElementById("btn4").innerHTML = "<a id='vinculo' href='test/reto.html' target='iframe4'>4. Reto Piramede</a>"
+}
+function ej4_5(){
+    document.getElementById("btn4").innerHTML = "<a id='vinculo' href='test/testalumno.html' target='iframe4'>5. Test Alumno</a>"
+}
